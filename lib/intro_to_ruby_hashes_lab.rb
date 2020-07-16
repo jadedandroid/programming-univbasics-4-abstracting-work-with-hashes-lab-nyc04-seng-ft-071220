883b2a7684c3a1a7a5f5d4e46ea3af2ab0225a94
jade decordova <lacroixjaded@gmail.com>
2020-07-16 22:18:14 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -5,8 +5,8 @@ def my_hash_creator(key, value)
 end
 
 def read_from_hash(hash, key)
-   if hash[:key]
-      return hash[:key]
+   if hash[key]
+      return hash[key]
     else
       nil
     end
